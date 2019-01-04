1f68d91a0fbf8745be4aa6cae85bd8ff513759b1
couzuwugezi <493749283@qq.com>
2019-01-04 14:45:23 +0800

    新建module 消费者

--- spring-cloud-parent/spring-cloud-consumer8082/src/main/java/com/springboot/ConsumerApplication.java
+++ spring-cloud-parent/spring-cloud-consumer8082/src/main/java/com/springboot/ConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author liqiang
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.springcloud.dao")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
