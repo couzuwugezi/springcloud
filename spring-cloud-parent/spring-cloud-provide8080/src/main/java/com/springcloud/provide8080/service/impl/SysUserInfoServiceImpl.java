e3b76a49becd46a1a69c49c0bda38e5cbc02b9ba
couzuwugezi <493749283@qq.com>
2019-01-27 20:59:30 +0800

    Eureka 集群配置

---
+++ spring-cloud-parent/spring-cloud-provide8080/src/main/java/com/springcloud/provide8080/service/impl/SysUserInfoServiceImpl.java
@@ -0,0 +1,91 @@
+package com.springcloud.provide8080.service.impl;
+
+import com.springcloud.dao.SysUserInfoDao;
+import com.springcloud.entity.SysUserInfo;
+import com.springcloud.provide8080.service.SysUserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 用户信息表(SysUserInfo)表服务实现类
+ *
+ * @author makejava
+ * @since 2019-01-04 15:47:33
+ */
+@Service("sysUserInfoService")
+@Transactional(rollbackFor = Exception.class)
+public class SysUserInfoServiceImpl implements SysUserInfoService {
+
+    @Autowired
+    private SysUserInfoDao sysUserInfoDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public SysUserInfo queryById(Integer id) {
+        return sysUserInfoDao.queryById(id);
+    }
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<SysUserInfo> queryAllByLimit(int offset, int limit) {
+        return sysUserInfoDao.queryAllByLimit(offset, limit);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param sysUserInfo 实例对象
+     * @return 是否成功
+     */
+    @Override
+    public boolean insert(SysUserInfo sysUserInfo) {
+        return sysUserInfoDao.insert(sysUserInfo) > 0;
+    }
+
+    /**
+     * 根据给定字段查询数据
+     *
+     * @param sysUserInfo 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public List<SysUserInfo> queryAll(SysUserInfo sysUserInfo) {
+        return sysUserInfoDao.queryAll(sysUserInfo);
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param sysUserInfo 实例对象
+     * @return 是否成功
+     */
+    @Override
+    public boolean update(SysUserInfo sysUserInfo) {
+        return sysUserInfoDao.update(sysUserInfo) > 0;
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Integer id) {
+        return sysUserInfoDao.deleteById(id) > 0;
+    }
+}
---
+++ spring-cloud-parent/spring-cloud-provide8081/src/main/java/com/springcloud/provide8081/controller/SysUserInfoController.java
@@ -0,0 +1,70 @@
+package com.springcloud.provide8081.controller;
+
+import com.springcloud.entity.SysUserInfo;
+import com.springcloud.provide8081.service.SysUserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 用户信息表(SysUserInfo)表控制层
+ *
+ * @author makejava
+ * @since 2019-01-04 15:58:31
+ */
+@RestController
+public class SysUserInfoController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private SysUserInfoService sysUserInfoService;
+
+    @Autowired
+    private DiscoveryClient client;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("/user/get/{id}")
+    public SysUserInfo selectOne(@PathVariable("id") Integer id) {
+        return this.sysUserInfoService.queryById(id);
+    }
+
+    @GetMapping(value = "/user/list")
+    public List<SysUserInfo> list() {
+        return sysUserInfoService.queryAll(new SysUserInfo());
+    }
+
+    @PostMapping(value = "/user/add")
+    public boolean add(SysUserInfo sysUserInfo) {
+        return sysUserInfoService.insert(sysUserInfo);
+    }
+
+    @GetMapping(value = "/provide8080/discovery")
+    public Object discovery() {
+//        List<String> services = client.getServices();
+//        List<String> collect = services.stream().filter("PROVIDE8080"::equals).collect(Collectors.toList());
+
+        List<ServiceInstance> serverList = client.getInstances("PROVIDE8080");
+        serverList.forEach(element -> System.out.println(element.getServiceId() + "\t"
+                + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri()));
+
+        return this.client;
+    }
+
+    @GetMapping(value = "/check/jiqun")
+    public String check() {
+        return "我是8081";
+    }
+}
