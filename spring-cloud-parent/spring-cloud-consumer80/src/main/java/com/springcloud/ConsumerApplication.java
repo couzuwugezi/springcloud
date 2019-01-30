9d48dde8db4a70286b980299a719af145f9829c0
couzuwugezi <493749283@qq.com>
2019-01-30 10:26:53 +0800

    ribbon Eureka 整合 初步配置

--- spring-cloud-parent/spring-cloud-consumer80/src/main/java/com/springcloud/ConsumerApplication.java
+++ spring-cloud-parent/spring-cloud-consumer80/src/main/java/com/springcloud/ConsumerApplication.java
@@ -3,10 +3,15 @@ package com.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author liqiang
+ * <p>
+ *     @EnableEurekaClient 消费者也作为一个Eureka客户端
+ * </p>
  */
+@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ConsumerApplication {
 
--- spring-cloud-parent/spring-cloud-consumer80/src/main/java/com/springcloud/consumer/config/ConfigBean.java
+++ spring-cloud-parent/spring-cloud-consumer80/src/main/java/com/springcloud/consumer/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.springcloud.consumer.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +13,15 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 
+    /**
+     * <p>
+     *     @LoadBalanced 客户端负载均衡
+     *     spring cloud ribbon 是一套客户端的负载均衡工具
+     * </p>
+     * @return
+     */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
